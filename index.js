c8fca93343786d846c4cbb2bb886e1208e9194e3
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-04-11 18:40:05 +0500

    Project Initialized

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+
+
 inquirer.prompt([
     {
         message: 'Type URL to generate QR code',
@@ -7,7 +12,12 @@ inquirer.prompt([
   
 ])
 .then((answers) => {
-    console.log('answers:', answers);
+
+    const url = answers.URL;
+    const name = url.match(/\/\/(?:www\.)?([^\.]+)/)[1];
+    const qr_code = qr.image(url);
+    qr_code.pipe(fs.createWriteStream(`${name}.png`));
+    
 })
 .catch((error) => {
     if (error.isTtyError) {
